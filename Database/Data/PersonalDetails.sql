67eafe56c25630e77251143054f3d56b6fe3bdc3
Rajani Raju <rajaniraju2003@gmail.com>
2021-01-25 17:20:57 -0600

    Database Added

---
+++ Database/Data/PersonalDetails.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO PersonalInformation (FirstName,LastName,Address,City,State,Zip)
+VALUES
+('Yolanda', 'Deluna', '3121 Stratford Lane SW', 'CR', 'Iowa', '52404'),
+('Janelle',  'Purdy', '3122 Stratford Lane SW' ,'CR', 'Iowa', '52404'),
+('Dennis', 'Purdy', '3122 Stratford Lane SW', 'CR', 'Iowa', '52404');
---
+++ Database/Tables/PersonalDetails.sql
@@ -0,0 +1,21 @@
+
+/****** Object:  Table [dbo].[PersonalInformation]    Script Date: 1/25/2021 5:17:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[PersonalInformation](
+	[FirstName] [nchar](20) NOT NULL,
+	[LastName] [nchar](20) NOT NULL,
+	[Address] [nvarchar](max) NOT NULL,
+	[Address2] [nchar](20) NULL,
+	[State] [nchar](10) NULL,
+	[City] [nchar](10) NULL,
+	[Zip] [nchar](10) NULL,
+	[Phone] [nchar](10) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
