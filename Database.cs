872366b2db07c1135769e4b7850f835d6b6eb63d
Rajani Raju <rajaniraju2003@gmail.com>
2021-01-25 22:56:41 -0600

    not working just created

---
+++ Database.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Data.SqlClient;
+
+namespace TestApp2
+{
+    public class Database
+    {
+        SqlConnection _cnn;
+        Database()
+        {
+            string connetionString = @"Server=WIN-50GP30FGO75;Database=Demodb;User ID=sa;Password=demol23";
+            _cnn = new SqlConnection(connetionString);
+        }
+    }
+}
