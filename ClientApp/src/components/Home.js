4312b1f80ac7190ec1b4d2d29baf2b90c6d276f9
James Poulose <james.poulose@gmail.com>
2021-02-14 20:41:59 -0600

    On edit go to details

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,74 +1,79 @@
 import React, { Component } from "react";
 import "./Home.css";
 import { Row } from "./row.js";
-//import {withRouter}
+import { withRouter } from "react-router-dom";
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  portNumber = "44300";
+    portNumber = "5001";
 
-  constructor(props) {
-    super(props);
-    this.tableRef = React.createRef();
-  }
+    constructor(props) {
+        super(props);
+        this.tableRef = React.createRef();
+    }
 
-  state = {
-    firstName: "",
-    lastName: "",
-    address: "",
-    address2: "",
-    city: "",
-    state: "",
-    zip: "",
-    phone: "",
-    id: "",
-    peopleArray: [],
-    displayList: [],
-  };
+    state = {
+        firstName: "",
+        lastName: "",
+        address: "",
+        address2: "",
+        city: "",
+        state: "",
+        zip: "",
+        phone: "",
+        id: "",
+        peopleArray: [],
+        displayList: [],
+    };
 
-  componentDidMount() {
-    this.getPersonList();
-    //when the window is up person list is loaded.
-    //Can also put this statement in constructor.
-  }
+    componentDidMount() {
+        this.getPersonList();
+        //when the window is up person list is loaded.
+        //Can also put this statement in constructor.
+    }
 
-  onEditUserEntry = (params) => {
-    let idToEdit = params.id;
-    console.log(idToEdit);
-  };
+    nextPath(path) {
+        this.props.history.push(path);
+    }
 
-  getPersonList = () => {
-    fetch(`https://localhost:${this.portNumber}/WeatherForecast/GetPersonList/`)
-      .then((res) => res.text())
-      .then((text) => {
-        return text.length ? JSON.parse(text) : null;
-      })
-      .then((personList) => {
-        console.log("#100", personList);
-        if (personList) {
-          this.setState({ peopleArray: personList });
-        }
-      });
-  };
-  onRowDeleted = (params) => {
-    console.log("from row:", params);
+    onEditUserEntry = (params) => {
+        let idToEdit = params.id;
+        console.log(idToEdit);
+        this.nextPath("/details");
+    };
 
-    fetch(`https://localhost:${this.portNumber}/WeatherForecast/DeleteEntry`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(params.id),
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-        this.setState({ peopleArray: result });
-      });
-  };
+    getPersonList = () => {
+        fetch(`https://localhost:${this.portNumber}/WeatherForecast/GetPersonList/`)
+            .then((res) => res.text())
+            .then((text) => {
+                return text.length ? JSON.parse(text) : null;
+            })
+            .then((personList) => {
+                console.log("#100", personList);
+                if (personList) {
+                    this.setState({ peopleArray: personList });
+                }
+            });
+    };
+    onRowDeleted = (params) => {
+        console.log("from row:", params);
 
- /* deleteUserEntry = (e) => {
+        fetch(`https://localhost:${this.portNumber}/WeatherForecast/DeleteEntry`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(params.id),
+        })
+            .then((res) => res.json())
+            .then((result) => {
+                console.log(result);
+                this.setState({ peopleArray: result });
+            });
+    };
+
+    /* deleteUserEntry = (e) => {
     e.preventDefault();
     let table = this.tableRef.current;
     const idIndex = 9;
@@ -101,46 +106,46 @@ export class Home extends Component {
       });
   };*/
 
-  render() {
-    let rows = this.state.peopleArray.map((people, index) => {
-      //console.log(index);
-      return (
-        <Row
-          key={index}
-          people={people}
-          deleteCurrentRow={this.onRowDeleted}
-          editCurrentRow={this.onEditUserEntry}
-        >
-          {" "}
-        </Row>
-      );
-    });
-    return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <div className="col s12 board">
-              <table ref={this.tableRef} style={{ width: "100%" }}>
-                <tbody>
-                  <tr>
-                    <th></th>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Address</th>
-                    <th>Address2</th>
-                    <th>State</th>
-                    <th>City</th>
-                    <th>Zip</th>
-                    <th>Phone</th>
-                    <th>id</th>
-                  </tr>
-                  {rows}
-                </tbody>
-              </table>
+    render() {
+        let rows = this.state.peopleArray.map((people, index) => {
+            //console.log(index);
+            return (
+                <Row
+                    key={index}
+                    people={people}
+                    deleteCurrentRow={this.onRowDeleted}
+                    editCurrentRow={this.onEditUserEntry}
+                >
+                    {" "}
+                </Row>
+            );
+        });
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col s12 board">
+                            <table ref={this.tableRef} style={{ width: "100%" }}>
+                                <tbody>
+                                    <tr>
+                                        <th></th>
+                                        <th>Firstname</th>
+                                        <th>Lastname</th>
+                                        <th>Address</th>
+                                        <th>Address2</th>
+                                        <th>State</th>
+                                        <th>City</th>
+                                        <th>Zip</th>
+                                        <th>Phone</th>
+                                        <th>id</th>
+                                    </tr>
+                                    {rows}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
