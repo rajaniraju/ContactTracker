e8a0fa0290acfe7a0765a8797ee7c1f2b37be714
Rajani Raju <rajaniraju2003@gmail.com>
2021-02-19 10:40:09 -0600

    Removed unused namespaces

--- Common/Person.cs
+++ Common/Person.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TestApp2.Common
+﻿namespace TestApp2.Common
 {
     public class Person
     {
--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,10 +4,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using TestApp2.Common;
 using System.Data;
-using System.Data.SqlClient;
 
 namespace TestApp2.Controllers
 {
