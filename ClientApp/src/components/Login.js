0388c935ebd2755f86cbfc713b25b43d34f0efa2
Rajani Raju <rajaniraju2003@gmail.com>
2021-02-26 11:06:58 -0600

    Handle on change added

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,43 +1,72 @@
 import React, { Component } from "react";
-import "./Login.css"
+import "./Login.css";
 import { Button, Col, Form, Alert } from "react-bootstrap";
-import {NavMenu} from "./NavMenu.js"
+import { NavMenu } from "./NavMenu.js";
 export class Login extends Component {
 	static displayName = Login.name;
 	portNumber = "44300";
+	constructor(props) {
+		super(props);
+		this.state = {
+			userName: "",
+			password: "",
+		};
+	}
+	handleUserInfo = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		this.setState({ [name]: value }, () => {
+			console.log(this.state.userName, this.state.password);
+		});
+	};
 
 	render() {
-        return (
-            <div>
-                <header>
-                    <NavMenu/>
-                </header>
-			<div className="Login">
-				<Form className="Login form">
-					<Form.Row>
-						<Form.Group as={Col} controlId="formGridfName">
-							<Form.Label >UserName</Form.Label>
-							<Form.Control type="UserName" name="UserName" />
-						</Form.Group>
-					</Form.Row>
-					<Form.Row>
-						<Form.Group as={Col} controlId="formGridlName">
-							<Form.Label>Password</Form.Label>
-							<Form.Control type="Password" name="Password" />
-						</Form.Group>
-					</Form.Row>
-					<Form.Row>
-						<Button block size="lg" type="submit" >Login</Button>
-					</Form.Row>
-					<Form.Row>
-                    <Alert.Link href={"#"}> Forgot Password ?  Forgot UserName ?</Alert.Link> 
-					</Form.Row>
-                    <Form.Row>
-                        <Alert.Link href={"#"}>New User</Alert.Link>
-					</Form.Row>
-				</Form>
-                </div>
-                </div>
+		return (
+			<div>
+				<header>
+					<NavMenu />
+				</header>
+				<div className="Login">
+					<Form className="Login form">
+						<Form.Row>
+							<Form.Group as={Col} controlId="formGridfName">
+								<Form.Label>UserName</Form.Label>
+								<Form.Control
+									type="userName"
+									name="userName"
+									value={this.state.userName}
+									onChange={this.handleUserInfo}
+								/>
+							</Form.Group>
+						</Form.Row>
+						<Form.Row>
+							<Form.Group as={Col} controlId="formGridlName">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type="password"
+									name="password"
+									value={this.state.password}
+									onChange={this.handleUserInfo}
+								/>
+							</Form.Group>
+						</Form.Row>
+						<Form.Row>
+							<Button block size="lg" type="submit">
+								Login
+							</Button>
+						</Form.Row>
+						<Form.Row>
+							<Alert.Link href={"#"}>
+								{" "}
+								Forgot Password ? Forgot UserName ?
+							</Alert.Link>
+						</Form.Row>
+						<Form.Row>
+							<Alert.Link href={"#"}>New User</Alert.Link>
+						</Form.Row>
+					</Form>
+				</div>
+			</div>
 		);
 	}
 }
